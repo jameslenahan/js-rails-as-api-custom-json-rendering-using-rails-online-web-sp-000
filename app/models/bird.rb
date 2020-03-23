0a281886bba04a7af9730a4e000a6e51f01d92e8
James Lenahan <james.k.lenahan@gmail.com>
2020-03-23 18:54:33 +0000

    Automatically backed up by Learn

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -1,2 +1,4 @@
-class Bird < ApplicationRecord
+class Bird < ApplicationRecorddef show
+  bird = Bird.find_by(id: params[:id])
+  render json: {id: bird.id, name: bird.name, species: bird.species } 
 end
